8a01fdded9bd7665fffc6a2ce602e5b6b3e43dbe
Joerg-Seitz <joerg.seitz0@gmail.com>
2016-11-01 16:14:50 +0100

    create ne template form for article and routes

--- alpha_blog/app/models/article.rb
+++ alpha_blog/app/models/article.rb
@@ -2,12 +2,5 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 3, maximum: 500}
 
-  a=5
-  b=7
-  a+b
-  
-
-#just for git testing reasons
-
 
 end
