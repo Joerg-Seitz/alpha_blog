f7750dc57a47555e2eed1ad998626c72965d92af
Joerg-Seitz <joerg.seitz0@gmail.com>
2016-11-04 12:52:13 +0100

    linking between articles pages and article#index created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles =Article.all
   end
 
   def article
@@ -10,12 +11,27 @@ class ArticlesController < ApplicationController
       @article=Article.new
   end
 
+  def  edit
+    @article =Article.find(params[:id])
+  end
+
+  def update
+    @article=Article.find(params[:id])
+    if @article.update (article_params)
+      flash[:notice]= "Article was succesfully updated!"
+      redirect_to article_path(@article)
+
+    else
+        render "edit"
+    end
+  end
+
   def create
 
     @article=Article.new(article_params)
     if @article.save
       redirect_to article_path(@article)
-      
+
     else
         render "new"
     end
